7ddac70531c09d2cffecb06dd6e7b415150a427a
TunaLagoona <maslova.wrk@gmail.com>
2020-05-27 18:13:07 +0300

    Create table in PostgreSQL

---
+++ initialize_table.sql
@@ -0,0 +1,23 @@
+CREATE TABLE opensky_state_vectors (
+    request_time INTEGER,
+    icao24 VARCHAR,
+    callsign VARCHAR,
+    origin_country VARCHAR,
+    time_position INTEGER,
+    last_contact INTEGER,
+    longitude DOUBLE PRECISION,
+    latitude DOUBLE PRECISION,
+    baro_altitude DOUBLE PRECISION,
+    on_ground BOOLEAN,
+    velocity DOUBLE PRECISION,
+    true_track DOUBLE PRECISION,
+    vertical_rate DOUBLE PRECISION,
+    sensors INTEGER,
+    geo_altitude DOUBLE PRECISION,
+    squawk TEXT,
+    spi BOOLEAN,
+    position_source INTEGER
+);
+
+
+
