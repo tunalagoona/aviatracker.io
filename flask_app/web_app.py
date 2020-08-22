7bb83238ed312044b4ad040e28ec1a6d68ea0f67
TunaLagoona <maslova.wrk@gmail.com>
2020-08-22 18:01:23 +0300

    Add flask_app folder

---
+++ flask_app/web_app.py
@@ -0,0 +1,76 @@
+import threading
+import time
+from contextlib import closing
+
+from flask import Flask
+from flask_socketio import SocketIO, send
+import eventlet
+import psycopg2
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode='eventlet')
+
+eventlet.monkey_patch()
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@socketio.on('connect')
+def connect():
+    print('go to http://localhost:5000')
+    print('connected')
+
+
+@socketio.on('disconnect')
+def disconnect():
+    print('disconnected')
+
+
+inter_states = [0]
+
+
+def fetch_vectors():
+    print('2nd thread has taken control')
+    with closing(psycopg2.connect(dbname="opensky", user='mas5mk', password='$GV9^MJGk8gn')) as conn:
+        with conn.cursor() as curs:
+            while True:
+                curs.execute("SELECT * FROM opensky_state_vectors "
+                             "WHERE request_time = (SELECT MAX(request_time) FROM opensky_state_vectors);")
+                vectors = curs.fetchall()
+                inter_states[0] = vectors
+                print(inter_states)
+                time.sleep(5)
+
+
+def broadcast_vectors():
+    while True:
+        # print('broadcast_vectors greenthread has taken control')
+        socketio.send(inter_states[0])
+        eventlet.sleep(4)
+        time.sleep(1)
+
+
+def start_app():
+    # print('start_app greenthread has taken control')
+    socketio.run(app)
+    eventlet.sleep(0.1)
+
+
+if __name__ == '__main__':
+    fetching_thread = threading.Thread(target=fetch_vectors, daemon=True)
+    fetching_thread.start()
+    broadcasting_greenthread = eventlet.spawn(broadcast_vectors)
+    app_launch_greenthread = eventlet.spawn(start_app)
+    # print('returned to the main green thread of the thread 1')
+    app_launch_greenthread.wait()
+    broadcasting_greenthread.wait()
+    fetching_thread.join()
+
+
+
+
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import config
 import yaml
 import setup_logging as log
 from opensky_api import StateVector, OpenskyStates
-from database import DbConnection
+from database_update import DbConnection
 from psycopg2 import DatabaseError
 from contextlib import closing
 from typing import List, Dict
@@ -31,7 +31,7 @@ class OpenskyDataExtraction:
     def insert_state_vectors_to_db(self, cur_time):
         try:
             logger.info('Connecting to the PostgreSQL database...')
-            with closing(DbConnection(dbname="openskydb", user=config.pg_username, password=config.pg_password)) as db:
+            with closing(DbConnection(dbname="opensky", user=config.pg_username, password=config.pg_password)) as db:
                 logger.info('Successful connection to the PostgreSQL database')
                 while True:
                     states = self.get_state_vectors(cur_time)
@@ -43,41 +43,10 @@ class OpenskyDataExtraction:
         except (Exception, DatabaseError):
             logger.exception("Exception in main(): ")
             self.insert_state_vectors_to_db(cur_time=cur_time)
-        # try:
-        #     logger.info('Connecting to the PostgreSQL database...')
-        #     db = DbConnection(dbname="openskydb", user=config.pg_username, password=config.pg_password)
-        #     logger.info('Successful connection to the PostgreSQL database')
-        #
-        #     api = OpenskyStates()
-        #
-        #     while True:
-        #         os_states = api.get_states(epoch_time=cur_time)
-        #         request_time = os_states["time"]
-        #         current_states = os_states["states"]
-        #         states = []
-        #
-        #         for state in current_states:
-        #             state_vector = StateVector(state, request_time)
-        #             states.append(state_vector)
-        #
-        #         db.upsert_state_vectors(states)
-        #         # new cursor is created for a batch of states, transaction is committed after a batch upserted
-        #
-        #         logger.debug(f'State vectors upserted: {states}')
-        #
-        #         cur_time += 5
-        #
-        # except (Exception, DatabaseError):
-        #     logger.exception("Exception in main(): ")
-        #     connect_to_api_and_insert_data_to_db(cur_time=cur_time)
-        #
-        # finally:
-        #     if db.conn is not None:
-        #         db.close()
-        #         logger.info('Database connection closed.')
 
 
 logger = log.setup()
-data_extraction = OpenskyDataExtraction()
-data_extraction.insert_state_vectors_to_db(cur_time=int(time.time()))
 
+if __name__ == "__main__":
+    data_extraction = OpenskyDataExtraction()
+    data_extraction.insert_state_vectors_to_db(cur_time=int(time.time()))
