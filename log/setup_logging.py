14595cac11e7a824bf30adf5cb1a00c5ef2674c5
TunaLagoona <maslova.wrk@gmail.com>
2020-10-28 16:19:46 +0300

    Put logging related files into one package named "log"

--- log/setup_logging.py
+++ log/setup_logging.py
@@ -1,17 +1,19 @@
 import logging
 import logging.config
+
 import yaml
 
 
-def setup(path='logging_config.yaml', level=logging.INFO):
+def setup(path='/Users/mas5mk/Work/Flight-tracker/log/logging_config.yaml', level=logging.INFO):
     with open(path, 'rt') as f:
         try:
             configurations = yaml.safe_load(f.read())
             logging.config.dictConfig(configurations)
-            # print(f'Logging configurations implemented successfully.')
+            # print(f'Logging configurations have been implemented successfully.')
         except Exception as e:
             print(e)
             print('Error in Logging Configuration. Using default configs')
             logging.basicConfig(level=level)
     log = logging.getLogger(__name__)
+    log.info('logger was initiated')
     return log
