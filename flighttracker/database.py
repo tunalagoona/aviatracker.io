e49423e7a386431cc6d8af74eded76c8ef3c7ed2
TunaLagoona <maslova.wrk@gmail.com>
2021-02-11 14:15:25 +0000

    Add methods for insertion and updating of paths

--- flighttracker/database.py
+++ flighttracker/database.py
@@ -186,5 +186,97 @@ class DB:
 
             return vectors, state_vectors_quantity
 
+    def insert_new_path(self, state, flight_info):
+        last_update = state[0]
+        icao24 = state[1]
+        first_location = {
+            'longitude': state[6],
+            'latitude': state[7]
+        }
+        path = [json.dumps(first_location)]
+        finished = False
+
+        flight_info["last_update"] = last_update
+        flight_info["icao24"] = icao24
+        flight_info["path"] = path
+        flight_info["finished"] = finished
+
+        with self.conn.cursor() as curs:
+            curs.execute(
+                """
+                    INSERT INTO flight_paths (last_update, icao24, departure_airport_icao, arrival_airport_icao,
+                    arrival_airport_long, arrival_airport_lat, estimated_arrival_time, path, finished)
+                    VALUES (%(last_update)s, %(icao24)s, %(departure_airport_icao)s, %(arrival_airport_icao)s,
+                    %(arrival_airport_long)s, %(arrival_airport_lat)s, %(estimated_arrival_time)s, %(path)s, %(finished)s)
+                """, flight_info
+            )
+
+    def update_paths(self):
+        with self.conn.cursor() as curs:
+            curs.execute(
+                "SELECT * FROM current_states;"
+            )
+            states = curs.fetchall()
+            time = states[0]
+
+            api = OpenskyStates()
+            airports_response = api.get_airports(begin= time - 5, end= time + 5)
+
+            for state in states:
+                curs.execute(
+                    "SELECT * FROM flight_paths "
+                    "WHERE icao24 = state[1];"
+                )
+                paths = curs.fetchall()
+
+                for item in airports_response:
+                    if item[0] == state[1]:
+                        departure_airport_icao = item[1]
+                        arrival_airport_icao = item[2]
+                        estimated_arr_time = item[3]
+                        airport_icao = '"' + arrival_airport_icao + '"'
+                        curs.execute(
+                            "SELECT latitude, longitude FROM airports"
+                            "WHERE icao = airport_icao;"
+                        )
+                        airport = curs.fetchone()
+                        arrival_airport_lat = airport[0]
+                        arrival_airport_long = airport[1]
+                        break
+
+                flight_info = {
+                    "departure_airport_icao": departure_airport_icao,
+                    "arrival_airport_icao": arrival_airport_icao,
+                    "arrival_airport_long": arrival_airport_long,
+                    "arrival_airport_lat": arrival_airport_lat,
+                    "estimated_arrival_time": estimated_arr_time
+                }
+
+                if len(paths) == 0:
+                    self.insert_new_path(state, flight_info)
+                else:
+                    latest_update = 0
+                    max_ind = 0
+                    for i in range(0, len(paths)):
+                        if paths[i][1] > latest_update:
+                            latest_update = paths[i][1]
+                            max_ind = i
+
+                    if paths[max_ind][9] is True:
+                        self.insert_new_path(state, flight_info)
+                    else:
+                        current_location = {
+                            'longitude': state[6],
+                            'latitude': state[7]
+                        }
+                        add_path = [json.dumps(current_location)]
+
+                        update_sql = """
+                            UPDATE flight_paths
+                            SET path = path || %s::jsonb
+                            WHERE icao24 = state[1] AND last_update = latest_update
+                        """
+                        curs.execute(update_sql, add_path)
+
     def close(self):
         self.conn.close()
