30bf45cacdcaeb69c2f5927c3e8a5db84ba02260
TunaLagoona <maslova.wrk@gmail.com>
2021-02-16 16:13:56 +0000

    Refactor Database initialization

--- flighttracker/scripts/init_database.py
+++ flighttracker/scripts/init_database.py
@@ -1,20 +1,37 @@
+import os
 from contextlib import closing
 
 import click
+import yaml
 
-from flighttracker.parser import Config
+from flighttracker.config import common_conf
 from flighttracker.database import Airport
 from flighttracker.database.database import DB
 
 
-@click.command(name='fill-airports')
-@click.option('--file', required=True, help='airport data file', type=str)
+@click.command(name="make-tables")
+def make_tables():
+    path = os.path.join(os.path.dirname(__file__), "make_tables.yaml")
+
+    with closing(DB(**common_conf.db_params)) as db:
+        with open(path, "r") as f:
+            for table_name in [
+                "current_states",
+                "flight_paths",
+                "airport_stats",
+                "airports",
+            ]:
+                script = yaml.load(f, Loader=yaml.FullLoader)[table_name]["create"]
+                db.execute_script(script)
+
+
+@click.command(name="fill-airports")
+@click.option("--file", required=True, help="airport data file", type=str)
 def fill_airports(file: str) -> None:
     with open(file) as f:
-        values = [line.split(',') for line in f.readlines()]
+        values = [line.split(",") for line in f.readlines()]
 
-    conf = Config()
-    with closing(DB(dbname=conf.db_name, user=conf.db_user, password=conf.db_pass, host=conf.db_host, port=conf.db_port)) as db:
+    with closing(DB(**common_conf.db_params)) as db:
         for value in values:
             db.insert(Airport(*value))
 
@@ -24,7 +41,7 @@ def cli():
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli.add_command(fill_airports)
 
     cli()
