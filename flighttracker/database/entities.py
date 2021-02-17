158c9b75ec33dc2c111d0bdc8f28759864d09bde
TunaLagoona <maslova.wrk@gmail.com>
2021-02-17 16:19:30 +0000

    Housekeeping

--- flighttracker/database/entities.py
+++ flighttracker/database/entities.py
@@ -1,5 +1,44 @@
 from collections import namedtuple
 
+StateVector = namedtuple(
+    "StateVector",
+    [
+        "request_time",
+        "icao24",
+        "callsign",
+        "origin_country",
+        "time_position",
+        "last_contact",
+        "longitude",
+        "latitude",
+        "baro_altitude",
+        "on_ground",
+        "velocity",
+        "true_track",
+        "vertical_rate",
+        "sensors",
+        "geo_altitude",
+        "squawk",
+        "spi",
+        "position_source",
+    ],
+)
+
+FlightPath = namedtuple(
+    "FlightPath",
+    [
+        "last_update",
+        "icao24",
+        "departure_airport_icao",
+        "arrival_airport_icao",
+        "arrival_airport_long",
+        "arrival_airport_lat",
+        "estimated_arrival_time",
+        "path",
+        "finished",
+    ],
+)
+
 Airport = namedtuple(
     "Airport",
     [
@@ -17,4 +56,9 @@ Airport = namedtuple(
         "item_type",
         "source",
     ],
+)
+
+FlightAirportInfo = namedtuple(
+    "FlightAirportInfo",
+    ["icao24", "estDepartureAirport", "estArrivalAirport", "estArrivalTime"],
 )
