e02794eac715090e3a11f2c3350a06d3589bdc76
TunaLagoona <maslova.wrk@gmail.com>
2021-02-18 12:51:06 +0000

    Refactor opensky module

--- flighttracker/opensky.py
+++ flighttracker/opensky.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import socket
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 from requests import exceptions, get
 
@@ -18,50 +18,46 @@ class OpenskyStates(object):
         self.auth = (username, password)
         self.api_url = "https://opensky-network.org/api"
 
-    def get_current_states(self, time_sec: int = 0, icao24: str = None) -> List[StateVector]:
-        parameters = {"time": int(time_sec), "icao24": icao24}
+    def get_from_opensky(self, params: Dict, operation: str) -> Dict:
         try:
             r = get(
-                "{}{}".format(self.api_url, "/states/all"),
+                "{}{}".format(self.api_url, operation),
                 auth=self.auth,
-                params=parameters,  # type: ignore
+                params=params,  # type: ignore
                 timeout=15,
             )
             if r.status_code == 200:
-                logger.info("Successful connection to Opensky API.")
+                logger.debug("Successful connection to Opensky API.")
                 response = json.loads(r.text)
-                request_time = response["time"]
-                dirty_states = response["states"]
-                states = [dict(StateVector(*([request_time] + state))) for state in dirty_states]
-                return states
+                return response
             else:
                 logger.error(f"Could not connect to Opensky API. Status code is {r.status_code}.")
+
         except (OSError, exceptions.ReadTimeout, socket.timeout) as e:
             logger.error(f"Could not get state vectors from API: {e}")
 
+    def get_current_states(self, time_sec: int = 0, icao24: str = None) -> List[StateVector]:
+        parameters = {"time": int(time_sec), "icao24": icao24}
+        operation = "/states/all"
+        resp = self.get_from_opensky(parameters, operation)
+
+        request_time = resp["time"]
+        dirty_states = resp["states"]
+        states = [dict(StateVector(*([request_time] + state))) for state in dirty_states]
+
+        return states
+
     def get_airports(self, begin: int, end: int) -> List[FlightAirportInfo]:
         parameters = {"begin": begin, "end": end}
-        try:
-            r = get(
-                "{}{}".format(self.api_url, "/flights/all"),
-                auth=self.auth,
-                params=parameters,  # type: ignore
-                timeout=15,
-            )
-            if r.status_code == 200:
-                logger.info("Successful connection to Opensky API.")
-                response = json.loads(r.text)
+        operation = "/flights/all"
+        resp = self.get_from_opensky(parameters, operation)
 
-                flight_airports = []
-                for flight in response:
-                    icao24 = flight["icao24"]
-                    dept_airport = flight["estDepartureAirport"]
-                    arrv_airport = flight["estArrivalAirport"]
-                    est_arr_time = flight["lastSeen"]
-                    flight_airports.append(FlightAirportInfo(*[icao24, dept_airport, arrv_airport, est_arr_time]))
+        flight_airports = []
+        for flight in resp:
+            icao24 = flight["icao24"]
+            dept_airport = flight["estDepartureAirport"]
+            arrv_airport = flight["estArrivalAirport"]
+            est_arr_time = flight["lastSeen"]
+            flight_airports.append(FlightAirportInfo(*[icao24, dept_airport, arrv_airport, est_arr_time]))
 
-                return flight_airports
-            else:
-                logger.error(f"Could not connect to Opensky API. Status code is {r.status_code}.")
-        except (OSError, exceptions.ReadTimeout, socket.timeout) as e:
-            logger.error(f"Could not get airports from API: {e}")
+        return flight_airports
