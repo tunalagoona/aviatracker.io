174e67dec7a9753bd9effea9bc67f61377dbde7d
TunaLagoona <maslova.wrk@gmail.com>
2021-02-24 10:45:58 +0000

    Fix circular import problem

--- aviatracker/database/__init__.py
+++ aviatracker/database/__init__.py
@@ -1,3 +1,3 @@
-from aviatracker.database.database import DB
 from aviatracker.database.entities import Airport, AirportStats, FlightAirportInfo, FlightPath, StateVector
 from aviatracker.database.utils import column_value_to_str
+from aviatracker.database.database import DB
