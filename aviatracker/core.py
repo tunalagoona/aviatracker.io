29c57ed108fddf7fed0dd39070ff69db54243bf5
TunaLagoona <maslova.wrk@gmail.com>
2021-02-25 12:54:28 +0000

    Change path update to use airports data from callsign_memo table

--- aviatracker/core.py
+++ aviatracker/core.py
@@ -4,11 +4,7 @@ from typing import List, Optional, Dict
 
 from aviatracker.database import DB
 from aviatracker.config import common_conf
-from aviatracker.opensky import OpenskyStates
-from aviatracker.database import (
-    FlightAirportInfo,
-    FlightPath
-)
+from aviatracker.database import FlightPath
 
 
 def update_flight_paths():
@@ -16,53 +12,39 @@ def update_flight_paths():
     with closing(DB(**params)) as db:
         with db:
             db.delete_outdated_paths()
-            db.update_finished_flag()
+            db.update_path_when_finished()
 
             states: Optional[List[Dict]] = db.get_current_states()
 
             if len(states) > 0:
-                cur_states_time = states[0]["request_time"]
-
-                api = OpenskyStates()
-                airports_response: List[FlightAirportInfo] = api.get_airports(
-                    begin=cur_states_time - 5, end=cur_states_time + 5
-                )
-
                 for state in states:
-                    unfinished_path: FlightPath = db.find_unfinished_path_for_aircraft(state["icao24"])
+                    icao = state["icao24"]
+                    callsign = state["callsign"]
+                    update_time = state["request_time"]
 
-                    for flight in airports_response:
-                        if flight["icao24"] == state["icao24"]:
-                            airport_long, airport_lat = db.get_airport_long_lat('"' + arrival_airport_icao + '"')
-
-                            departure_airport_icao = flight["estDepartureAirport"]
-                            arrival_airport_icao = flight["estArrivalAirport"]
-                            estimated_arr_time = flight["estArrivalTime"]
-                            arrival_airport_lat = airport_lat
-                            arrival_airport_long = airport_long
-                            break
+                    unfinished_path: FlightPath = db.find_unfinished_path_for_aircraft(icao)
 
                     current_location = {
                         "longitude": state["longitude"],
                         "latitude": state["latitude"],
                     }
                     path = [json.dumps(current_location)]
-                    icao = state["icao24"]
 
                     if len(unfinished_path) == 0:
+                        arrival_airport_icao, departure_airport_icao = db.get_airports_for_callsign(callsign)
+
                         new_path = FlightPath(
-                            last_update=state["request_time"],
+                            last_update=update_time,
                             icao24=icao,
+                            callsign=callsign,
                             departure_airport_icao=departure_airport_icao,
                             arrival_airport_icao=arrival_airport_icao,
-                            arrival_airport_long=arrival_airport_long,
-                            arrival_airport_lat=arrival_airport_lat,
-                            estimated_arrival_time=estimated_arr_time,
                             path=path,
                             finished=False,
                             finished_at=0
                         )
+
                         db.insert_path(new_path)
                     else:
                         last_update = unfinished_path["last_update"]
-                        db.update_unfinished_path(icao, last_update, path)
+                        db.update_unfinished_path(icao, last_update, path, update_time)
