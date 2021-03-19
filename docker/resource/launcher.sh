f71445756c2dc94ceec0aa6b93c83782ebfc9b05
TunaLagoona <maslova.wrk@gmail.com>
2021-03-19 11:58:36 +0000

    Housekeeping

--- docker/resource/launcher.sh
+++ docker/resource/launcher.sh
@@ -1,6 +1,6 @@
 rabbitmq-server -detached
 
-celery -A aviatracker.task_scheduling beat --detach -l INFO -f /aviatracker/logs/celery.log -s /aviatracker/beat/celerybeat-schedule
-celery -A aviatracker.task_scheduling worker --detach -l INFO -f /aviatracker/logs/celery.log --without-gossip --without-mingle
+celery -A aviatracker.tasks beat --detach -l INFO -f /logs/celery.log -s /aviatracker/beat/celerybeat-schedule
+celery -A aviatracker.tasks worker --detach -l INFO -f /logs/celery.log --without-gossip --without-mingle
 
 python3 -m aviatracker.web.app
