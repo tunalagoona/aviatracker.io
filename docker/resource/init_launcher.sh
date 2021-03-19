c440a8b61f99e2a75e096d7bdcd5103849581223
TunaLagoona <maslova.wrk@gmail.com>
2021-03-19 12:02:13 +0000

    Housekeeping

--- docker/resource/init_launcher.sh
+++ docker/resource/init_launcher.sh
@@ -1,3 +1,3 @@
 python3 -m aviatracker.scripts.init_database make-tables
-python3 -m aviatracker.scripts.init_database make-tables fill-callsigns
-python3 -m aviatracker.scripts.init_database make-tables fill-airports --file="/extra/airports.txt"
+python3 -m aviatracker.scripts.init_database fill-callsigns
+python3 -m aviatracker.scripts.init_database fill-airports --file="/extra/airports.txt"
