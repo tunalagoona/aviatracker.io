97660640f990c3bd6033f0e39134ffc4ed40e66c
TunaLagoona <maslova.wrk@gmail.com>
2021-03-22 12:42:52 +0000

    Start 3 celery workers consuming tasks from different queues

--- docker/resource/launcher.sh
+++ docker/resource/launcher.sh
@@ -1,6 +1,9 @@
 rabbitmq-server -detached
 
-celery -A aviatracker.tasks beat --detach -l INFO -f /logs/celery.log -s /aviatracker/beat/celerybeat-schedule
-celery -A aviatracker.tasks worker --detach -l INFO -f /logs/celery.log --without-gossip --without-mingle
+celery -A aviatracker.tasks beat --detach -l INFO -f logs/celery.log -s beat/celerybeat-schedule
+
+celery -A aviatracker.tasks worker --detach -l DEBUG -f logs/celery.log --without-gossip --without-mingle -n worker1@%h -Q states --concurrency=1
+celery -A aviatracker.tasks worker --detach -l INFO -f logs/celery.log --without-gossip --without-mingle -n worker2@%h -Q paths --concurrency=1
+celery -A aviatracker.tasks worker --detach -l INFO -f logs/celery.log --without-gossip --without-mingle -n worker3@%h -Q celery --concurrency=1
 
 python3 -m aviatracker.web.app
